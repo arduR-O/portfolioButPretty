23381671277c2afe3729eee724e43f68e1d31d6f
arduR-O <omaansh@gmail.com>
2023-08-16 22:45:16 +0530

    feat: primitive nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar';
-import Dotgroup from "./components/Dotgroup";
+// import Dotgroup from "./components/Dotgroup";
 
 function App() {
   //we want to keep track of the current section of page so that we can render that onto the navbar and dotgroup, so we declare states for the same, default is home
   const [currentPage, setCurrentPage] = useState("Home");
+  //we have this feature in navbar that needs to know whether the user is on the top of the page, so let's dig into that
+  const [isTopOfThePage, setIsTopOfThePage] = useState(true);
+  //by default user is on the top but that might change in future, let's handle that change using window api (a side effect)
+  //basically we are going to plant a listener to just keep track if the user is on the top
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      if(window.scrollY === 0){
+        //this means that user is at the top, in this case
+        setCurrentPage("Home");
+        setIsTopOfThePage(true);
+      }
+      else{
+        setIsTopOfThePage(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    //return the cleanup function
+    return window.removeEventListener("scroll", handleScroll);
+    //we are leaving dependency empty since we only need this to run once
+  },[])
 
   return (
     <>
     {/* we are going to use relative positioning to positioning all elements*/}
     {/* we pass currentPage and setCurrentPage to both navbar and dotgroup because depending on that they have certain graphics, also both components involve interactions that update the current page */}
-    <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage}/>
-    <DotGroup currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+    <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} isTopOfThePage/>
+    {/* <DotGroup currentPage={currentPage} setCurrentPage={setCurrentPage}/> */}
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
-const Navbar = ({currentPage, setCurrentPage}) => {
+import useMediaQuery from "../hooks/useMediaQuery";
+
+const Navbar = ({currentPage, setCurrentPage, isTopOfThePage}) => {
     //now this navbar is going to be a banner for desktop users and a togglable button for mobile users, so we need a conditional statement to take care of that, for that we are going to use this cool hook that we have made
+    const isDesktop = useMediaQuery("(min-width: 768px)"); //768px covers tablets and larger screens
+
+    //we also want that while the user is on the home section the navbar has no color but when he goes down the navbar gets some bg
+    const navBg = (isTopOfThePage)? "" : "bg-pink";
     
-    return (  );
+    
+    return ( 
+        // the layout that we desire for navbar is that we have website title OS on right and nav bar or toggle button on the left 
+        //the navBg var handles the bg feature, the relative, top stuff is for positioning, z for positioning on z axis
+        <nav className= {`${navBg} fixed z-40 top-0 h-auto w-full flex justify-between mx-7 px-5`}> 
+            <h4 className="font-playfair text-4xl">OS</h4>
+            <div className="flex gap-3">
+                <p className="font-playfair text-2xl">sampel1</p>
+                <p className="font-playfair text-2xl">sampel1</p>
+                <p className="font-playfair text-2xl">sampel1</p>
+            </div>
+        </nav>
+     );
 }
  
 export default Navbar;
