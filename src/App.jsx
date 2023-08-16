e88c2b38a930fa0709b65ea2e2d5ee8033530758
arduR-O <omaansh@gmail.com>
2023-08-16 22:13:22 +0530

    feat: added useMediaQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar';
+import Dotgroup from "./components/Dotgroup";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //we want to keep track of the current section of page so that we can render that onto the navbar and dotgroup, so we declare states for the same, default is home
+  const [currentPage, setCurrentPage] = useState("Home");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* we are going to use relative positioning to positioning all elements*/}
+    {/* we pass currentPage and setCurrentPage to both navbar and dotgroup because depending on that they have certain graphics, also both components involve interactions that update the current page */}
+    <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+    <DotGroup currentPage={currentPage} setCurrentPage={setCurrentPage}/>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+const Navbar = ({currentPage, setCurrentPage}) => {
+    //now this navbar is going to be a banner for desktop users and a togglable button for mobile users, so we need a conditional statement to take care of that, for that we are going to use this cool hook that we have made
+    
+    return (  );
+}
+ 
+export default Navbar;
