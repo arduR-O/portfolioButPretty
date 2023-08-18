c4d0f9c101f83cafabb4821d90038772d9e4e758
arduR-O <omaansh@gmail.com>
2023-08-18 11:59:53 +0530

    feat: added complete responsiveness to navbar

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,7 +1,8 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 //I want the link to do following things, here page is the page the link is linked to: 1) go to particular section when clicked upon and update currentPage 2) turn yellow on hover 3)be yellow if currentPage === page 
-const Link = ({page, currentPage, setCurrentPage}) => {
+//I am using style to handle when I want some additional styling in links
+const Link = ({page, currentPage, setCurrentPage, style}) => {
     //page is in title case, currentpage and ids in lowercase
     const lcpage = page.toLowerCase();
     //need to make a conditional statement for font color
@@ -10,7 +11,7 @@ const Link = ({page, currentPage, setCurrentPage}) => {
         <AnchorLink
         //remember how we used to internally link sections using ID?? that's exactly what we want to do right now, just that we are doing it using variables
         href={`#${lcpage}`}
-        className={`text-2xl ${fontCol} hover:text-yellow font-poppins py-3`}
+        className={`text-2xl ${fontCol} hover:text-yellow font-poppins py-3 ${style? style:""}`}
         onClick={()=>{setCurrentPage(lcpage)}}
         >{page}</AnchorLink>
       );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
 import Link from "./Link";
 
 const Navbar = ({ currentPage, setCurrentPage, isTopOfThePage }) => {
   //now this navbar is going to be a banner for desktop users and a togglable button for mobile users, so we need a conditional statement to take care of that, for that we are going to use this cool hook that we have made
   const isDesktop = useMediaQuery("(min-width: 768px)"); //768px covers tablets and larger screens
-  console.log(isDesktop);
   //we also want that while the user is on the home section the navbar has no color but when he goes down the navbar gets some bg
   const navBg = isTopOfThePage ? "" : "bg-yellow";
 
   //this one is going to be used to track the toggled state of menu button for mobile screens
   const [isToggled, setIsToggled] = useState(false);
+  console.log("isToggled: "+ isToggled);
+  //I have this feature for mobile screens in which the navbar collapses when there is a click outside it, so let me write code for that, since there are going to be sideeffect, event listeners and stuff, lemme use a useEffect hook
+  //first we need a reference to the nav element, so that we can access it
+  const navbarRef = useRef(null); //initially it is null, it will gain value once I mount it onto the required navbar
+  useEffect(()=>{
+    //define function to handle clicks
+    const handleOutsideClick = (e)=>{
+      //we want to check two things: 1) navbar is actually mounted in the dom (navbarRef.current!=null) 2) that click has originated out of navbar
+      if(navbarRef.current && !navbarRef.current.contains(e.target)){
+        //if these are fulfilled I wanna change the value of toggle so that the menu collapses
+        setIsToggled(false);
+      }
+    }
+    //add event listener to listen to clicks in whole website
+    document.addEventListener("click",handleOutsideClick);
+    return ()=>document.removeEventListener("click",handleOutsideClick);
+
+    //IMPORTANT NOTE: When I initially run this function, the toggle button won't work, what happens is that when I click that button it opens the menu, but then the click, which has been applied to whole document bubbles up and overwrites the change caused by the button click, that's why we need to stop propogation manually in the button so that it does not bubble up, and that fixes the issue
+  }, [])
 
   return (
     // the layout that we desire for navbar is that we have website title OS on right and nav bar or toggle button on the left
@@ -53,39 +71,45 @@ const Navbar = ({ currentPage, setCurrentPage, isTopOfThePage }) => {
           <img
             src="../assets/menu-icon.svg"
             alt="menu icon"
-            onClick={() => {
+            onClick={(e) => {
               setIsToggled(true);
+              e.stopPropagation();
             }}
           />
         </button>
       ) : (
-        <div className="fixed z-10 w-5/12 h-full top-0 right-0 bg-gradient-to-br from-cyan-400 to-indigo-600 flex flex-col">
+        <nav ref={navbarRef} className="fixed z-10 w-6/12 xs:w-5/12 h-full top-0 right-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col px-3 xs:px-10 justify-evenly">
           <Link
             page="Home"
             currentPage={currentPage}
             setCurrentPage={setCurrentPage}
+            style="font-semibold"
           />
           <Link
             page="Skills"
             currentPage={currentPage}
             setCurrentPage={setCurrentPage}
+            style="font-semibold"
           />
           <Link
             page="Projects"
             currentPage={currentPage}
             setCurrentPage={setCurrentPage}
+            style="font-semibold"
           />
           <Link
             page="Testimonials"
             currentPage={currentPage}
             setCurrentPage={setCurrentPage}
+            style="font-semibold"
           />
           <Link
             page="Contact"
             currentPage={currentPage}
             setCurrentPage={setCurrentPage}
+            style="font-semibold"
           />
-        </div>
+        </nav>
       )}
     </nav>
   );
